9bbe640c7e3a930ac596cbb6225a80d8ef26d2f8
administrador <administrador@administradors-iMac.local>
2018-12-03 12:22:16 -0800

    Primera clase

---
+++ MyPlayground01.playground/Contents.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Bye, playground"
+print(str)
+
+let abc = "clase de ios"
+print(abc)
+
+var num = 1.3
+num = num + 5
+print(num)
+
+let àl = "Simblo"
+print(àl)
+
+let 😇 = "angelito"
+print(😇)
+
+struct Person {
+    let firtsname : String
+    let lastname : String
+    
+    func sayHello () {
+        print("Hola! mi nombre es \(firtsname) \(lastname)")
+    }
+}
+
+let personaA = Person(firtsname: "cesar", lastname: "villacorta")
+let personaB = Person(firtsname: "fernando", lastname: "velasquez")
+
+print(personaA.sayHello())
+personaB.sayHello()
+
+let number : Double = 5
+print(number)
+
+let number2 : Int =	 5
+print(number2)
+
+let number3 = 5
+print(number3)
+
+//operador matematico
+
+/*
+ 
+ Operador matematico
+ 
+ */
+
+var score = 5
+score = score + 4
+print(score)
+score += 4
+score -= 3
+score *= 3
+score /= 10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
